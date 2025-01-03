16729bffadeff091e6a809f0be8ee443f7faf912
ChinKF99 <apb2657@gmail.com>
2025-01-03 19:00:51 +0800

    Create operation function which carry out +,-,*,/

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ function multiply(a,b){
 function divide(a,b){
     return a / b
 }
+
+function operation(numA,op,numB){
+    let x = op
+    switch (x){
+        case "+":
+            return add(numA,numB);
+            break;
+        case "-":
+            return subtract(numA,numB);
+            break;
+        case "*":
+            return multiply(numA,numB);
+            break;
+        case "/":
+            return divide(numA,numB);
+            break;
+        
+    }
+}
