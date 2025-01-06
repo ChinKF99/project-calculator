8b6778fbef98e4da2a5011f0ad619ced02097d28
ChinKF99 <apb2657@gmail.com>
2025-01-06 16:41:29 +0800

    Added event listener, to further code

--- main.js
+++ main.js
@@ -32,3 +32,44 @@ function operation(numA,op,numB){
         
     }
 }
+
+
+let ops = ""
+
+
+const numbers = document.querySelectorAll("#numbers")
+
+const operators = document.querySelectorAll("#operator")
+
+const results = document.querySelector("#result")
+
+const operations = document.querySelector("#operations")
+
+
+
+numbers.forEach(number => number.addEventListener("click",()=>{
+    if(ops === ""){
+        let numberOne = 0
+        numberOne = +number.value
+        results.value = numberOne
+        return numberOne
+    } else{
+        let numberTwo = 0
+        numberTwo = +number.value
+        results.value = numberTwo
+        return numberTwo
+    }
+}))
+
+operators.forEach(operator => operator.addEventListener("click", ()=>{
+    ops = operator.value
+    results.value = ops
+    return ops
+}))
+
+operations.addEventListener("click",()=>{
+    console.log(numberOne)
+    console.log(numberTwo)
+})
+
+
