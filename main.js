db45a519a38ee23ed9acf4a56097ad3f3d64facd
ChinKF99 <apb2657@gmail.com>
2025-01-07 19:32:48 +0800

    Added longScreen in Index.html Added style to longScreen Added many functons & comments in main.js
    *Currently able to perform 1 simple calculation

--- main.js
+++ main.js
@@ -15,61 +15,87 @@ function divide(a,b){
 }
 
 function operation(numA,op,numB){
-    let x = op
-    switch (x){
+    // Convert numA & numB to integer
+    x = Number(numA)
+    y = Number(numB)
+    switch (op){
         case "+":
-            return add(numA,numB);
+            return add(x,y);
             break;
         case "-":
-            return subtract(numA,numB);
+            return subtract(x,y);
             break;
         case "*":
-            return multiply(numA,numB);
+            return multiply(x,y);
             break;
         case "/":
-            return divide(numA,numB);
+            return divide(x,y);
             break;
-        
     }
 }
 
-
+let array = []
 let ops = ""
-
+let previousValue = ""
+let currentValue = ""
+let finalResult = ""
 
 const numbers = document.querySelectorAll("#numbers")
 
 const operators = document.querySelectorAll("#operator")
 
-const results = document.querySelector("#result")
+const screen = document.querySelector("#screen")
 
 const operations = document.querySelector("#operations")
 
+const longScreen = document.querySelector("#longScreen")
 
-
-numbers.forEach(number => number.addEventListener("click",()=>{
-    if(ops === ""){
-        let numberOne = 0
-        numberOne = +number.value
-        results.value = numberOne
-        return numberOne
-    } else{
-        let numberTwo = 0
-        numberTwo = +number.value
-        results.value = numberTwo
-        return numberTwo
-    }
+//Event listener to carry out a function if numbers is click 
+numbers.forEach((number) => number.addEventListener("click",(e)=>{
+    handleNumber(e.target.value)
+    screen.value = currentValue
 }))
 
-operators.forEach(operator => operator.addEventListener("click", ()=>{
-    ops = operator.value
-    results.value = ops
-    return ops
+//Event listener to carry out a function if operator is click 
+operators.forEach((operator) => operator.addEventListener("click", (e)=>{
+    handleOps(e.target.value)
+    array.push(currentValue)
+    //Display the previous Number until a new number is click on
+    screen.value = previousValue
+    //Display the current full equation
+    longScreen.value = previousValue + currentValue
+    //check for the array value to ensure all is in order
+    console.log(array)
 }))
 
+//Event listener to carry out a function if "=" is click 
 operations.addEventListener("click",()=>{
-    console.log(numberOne)
-    console.log(numberTwo)
+    // Display the full equation
+    longScreen.value = array.join("") + currentValue
+    finalResult = operation(array[0],array[1],currentValue)
+    //Display result at the screen
+    screen.value = finalResult
 })
 
+function handleNumber(num){
+    // Reset currentValue to "" so it can accomodate up to 5 numbers
+    if(previousValue){
+        previousValue = ""
+        currentValue = ""
+    }
+    if (currentValue.length <=5){
+    currentValue += num
+}}
+
+
+function handleOps(ops){
+    // Check if there is an existing operator, if so replace it in array and longScreen as well
+    if(array.length > 1){
+        currentValue = ops
+        array.pop()
+    }
+    else{previousValue = currentValue
+    array.push(previousValue)
+    currentValue = ops}
+}
 
