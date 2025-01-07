9e4ea39589fc7ce80deea93af840c51d8e4adcc5
Chin <khaix.foong.chin@intel.com>
2025-01-07 15:56:47 +0800

    Added function to handle numbers and operators.

--- main.js
+++ main.js
@@ -35,41 +35,43 @@ function operation(numA,op,numB){
 
 
 let ops = ""
-
+let previousValue = ""
+let currentValue = ""
 
 const numbers = document.querySelectorAll("#numbers")
 
 const operators = document.querySelectorAll("#operator")
 
-const results = document.querySelector("#result")
+const screen = document.querySelector("#screen")
 
 const operations = document.querySelector("#operations")
 
 
 
-numbers.forEach(number => number.addEventListener("click",()=>{
-    if(ops === ""){
-        let numberOne = 0
-        numberOne = +number.value
-        results.value = numberOne
-        return numberOne
-    } else{
-        let numberTwo = 0
-        numberTwo = +number.value
-        results.value = numberTwo
-        return numberTwo
-    }
+numbers.forEach((number) => number.addEventListener("click",(e)=>{
+    handleNumber(e.target.value)
+    screen.value = currentValue
 }))
 
-operators.forEach(operator => operator.addEventListener("click", ()=>{
-    ops = operator.value
-    results.value = ops
-    return ops
+operators.forEach((operator) => operator.addEventListener("click", (e)=>{
+    handleOps(e.target.value)
+    screen.value = currentValue
+    console.log(previousValue)
 }))
 
-operations.addEventListener("click",()=>{
-    console.log(numberOne)
-    console.log(numberTwo)
-})
+function handleNumber(num){
+    if (currentValue.length <=5){
+    currentValue += num
+}}
+
+function handleOps(ops){
+    previousValue = currentValue
+    currentValue = ops
+}
+
+// operations.addEventListener("click",()=>{
+//     console.log(numberOne)
+//     console.log(numberTwo)
+// })
 
 
